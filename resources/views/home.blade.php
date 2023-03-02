ac6779f61d0ae54ad2e848f3d74de6d747297a66
sandycotorina <tsandyco@gmail.com>
2023-03-02 08:22:13 +0700

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,28 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="d-flex">
-        <p style="margin-left: 2px; font-weight: bold;">Master Barang</p>
-        <p style="margin-left: 5px;">/ Tampilkan Stock</p>
+<body class="d-flex h-100 text-center text-bg-dark">
+
+<div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header class="mb-auto">
+    <div>
+      <h3 class="float-md-start mb-0">Welcome</h3>
     </div>
-<div class="col-lg-6 col-md-6 mx-auto">
-    <div class="card card-user">
-        <div class="image">
-        <img src="{{asset('asset/img/komputer.jpg')}}" width="100%" alt="...">
-        </div>
-    </div>
+  </header>
+
+  <main class="px-3">
+    <h1>To My Project</h1>
+    <p class="lead">Selamat Datang Di Project Inventory Saya, Oh yaa..jangan lupa like, comment, and follow yaaa, Ini adalah tampilan home sandy, beri rating yaa terimakasih teman teman.</p>
+    <img src="{{asset('asset/img/ganteng.jpg')}}" width="13%" alt="...">
+    <p class="lead" style="margin-top: 2%">
+        <a href="https://www.instagram.com/_sndyco" class="btn btn-lg btn-secondary">Instagram</a>
+    </p>
+  </main>
 </div>
-<table class="table" style="margin-top: 10%">
-    <thead>
-      <tr>
-        <th scope="col">Kode Barang</th>
-        <th scope="col">Nama Barang </th>
-        <th scope="col">Brand</th>
-        <th scope="col">UOM</th>
-        <th scope="col">Price</th>
-      </tr>
-    </thead>
-  </table>
+</body>
 @endsection
 
